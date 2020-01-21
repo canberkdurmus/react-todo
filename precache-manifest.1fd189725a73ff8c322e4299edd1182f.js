f9b6042c639245603a17da0c712d4a957b1d1c7f
canberk <canberkdurmus@gmail.com>
2020-01-21 13:07:23 +0300

    Updates

---
+++ precache-manifest.1fd189725a73ff8c322e4299edd1182f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac35bd68708b7c00d3a8e2c7632d899",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "b5b10a7107b279e768a7",
+    "url": "/react-todo/static/css/main.83d350e0.chunk.css"
+  },
+  {
+    "revision": "1438b932e95ccadc29d4",
+    "url": "/react-todo/static/js/2.20fde20d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-todo/static/js/2.20fde20d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5b10a7107b279e768a7",
+    "url": "/react-todo/static/js/main.f6adfc44.chunk.js"
+  },
+  {
+    "revision": "4637a77acbbb1bfa3441",
+    "url": "/react-todo/static/js/runtime-main.852ef8c9.js"
+  }
+]);
